32e9e5e7565470f74860041478785ae41485faf3
Kemal1603 <prince_of_love93@mail.ru>
2021-01-10 19:41:38 +0300

    lkj .j

--- main.py
+++ main.py
@@ -190,7 +190,7 @@ def about():
 
 def send_email(name, email, phone, message):
     email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
-    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(OWN_EMAIL, OWN_PASSWORD)
         connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
